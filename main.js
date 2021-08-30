d82a0df4d26e7e5e493a9d883149f01e64eb7365
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-08-30 18:45:10 +0100

    Fix scroll, Linters errors; Add animation to mobile menu button

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const menuBtn = document.querySelector('.hamburger-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+// Scrolling functions
+function disableScroll() {
+  document.body.classList.add('unscrollable');
+}
+function enableScroll() {
+  document.body.classList.remove('unscrollable');
+}
+
+// Handle click event on menu button
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('open');
-  let classMenu = mobileMenu.classList;
+  const classMenu = mobileMenu.classList;
   if (classMenu.contains('hide-on-mobile')) {
     classMenu.replace('hide-on-mobile', 'show-on-mobile');
-  } else {
+    disableScroll();
+  } else if (classMenu.contains('show-on-mobile')) {
     classMenu.replace('show-on-mobile', 'hide-on-mobile');
+    enableScroll();
   }
+  // Handle click event on links from mobile menu;
+  const links = mobileMenu.querySelectorAll('a');
+  links.forEach((l) => l.addEventListener('click', () => {
+    menuBtn.classList.remove('open');
+    classMenu.replace('show-on-mobile', 'hide-on-mobile');
+    enableScroll();
+  }));
 });
