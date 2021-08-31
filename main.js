71a239d3b4152e2135b7864be2f6d22e8c0a5c52
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-08-31 12:07:56 +0100

    Fix ESLint errors

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-const menuBtn = document.querySelector(".hamburger-icon");
-const mobileMenu = document.querySelector(".mobile-menu");
+const menuBtn = document.querySelector('.hamburger-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-// mobileMenu.style.display = "none";
+// mobileMenu.style.display = 'none';
 
 // Scrolling functions
 function disableScroll() {
-  document.body.classList.add("unscrollable");
+  document.body.classList.add('unscrollable');
 }
 function enableScroll() {
-  document.body.classList.remove("unscrollable");
+  document.body.classList.remove('unscrollable');
 }
 
 // Handle click event on menu button
-menuBtn.addEventListener("click", () => {
-  let menuBtnClasses = menuBtn.classList;
- 
-  menuBtnClasses.toggle("open");
+menuBtn.addEventListener('click', () => {
+  const menuBtnClasses = menuBtn.classList;
+  menuBtnClasses.toggle('open');
 
   const classMenu = mobileMenu.classList;
-  if (classMenu.contains("hide")) {
-    classMenu.replace("hide", "show-on-mobile");
+  if (classMenu.contains('hide')) {
+    classMenu.replace('hide', 'show-on-mobile');
     disableScroll();
-  } else if (classMenu.contains("show-on-mobile")) {
-    classMenu.replace("show-on-mobile", "hide");
+  } else if (classMenu.contains('show-on-mobile')) {
+    classMenu.replace('show-on-mobile', 'hide');
     enableScroll();
   }
   // Handle click event on links from mobile menu;
-  const links = mobileMenu.querySelectorAll("a");
-  links.forEach((l) =>
-    l.addEventListener("click", () => {
-      menuBtn.classList.remove("open");
-      classMenu.replace("show-on-mobile", "hide");
-      enableScroll();
-    })
-  );
+  const links = mobileMenu.querySelectorAll('a');
+  links.forEach((l) => l.addEventListener('click', () => {
+    menuBtn.classList.remove('open');
+    classMenu.replace('show-on-mobile', 'hide');
+    enableScroll();
+  }));
 });
