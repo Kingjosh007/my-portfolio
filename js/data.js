559581586be193537215db7dd336e3896cda105b
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-08-31 13:55:37 +0100

    Create projects array in data.js

---
+++ js/data.js
@@ -0,0 +1,26 @@
+const projects = [
+    {
+        "id": 1,
+        "title": "FootAIz",
+        "picture": "images/FootAIz.PNG",
+        "modal-picture": "assets/png/modal_image.png",
+        "description": "A software that makes football predictions based on artificial intelligence. Based on past data and various models, the software can make pretty accurate predictions in 16 differents football leagues in Europe.",
+        "technologies": ["node.js", "brain.js", "redis", "Adaboost"]
+    },
+    {
+        "id": 2,
+        "title": "ScrabbleCoach",
+        "picture": "images/scrabblecoach.PNG",
+        "modal-picture": "assets/png/modal_image.png",
+        "description": "An desktop application that helps scrabble players learning the words used in the french scrabble game (more than 400,000 words) from 2 to 15 letters long.",
+        "technologies": ["electron.js", "express.js", "indexedDB", "css"]
+    },
+    {
+        "id": 3,
+        "title": "Multi-Post Stories",
+        "picture": "assets/png/project_screenshot_placeholder.png",
+        "modal-picture": "assets/png/modal_image.png",
+        "description": "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+        "technologies": ["css", "html", "bootstrap", "Ruby"]
+    }
+];
