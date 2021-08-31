935b61819bb4cd5f6beaaaf7dc45836fd2a3627f
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-08-31 08:57:00 +0100

    Add animations to mobile menu

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
-const menuBtn = document.querySelector('.hamburger-icon');
-const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtn = document.querySelector(".hamburger-icon");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 // Scrolling functions
 function disableScroll() {
-  document.body.classList.add('unscrollable');
+  document.body.classList.add("unscrollable");
 }
 function enableScroll() {
-  document.body.classList.remove('unscrollable');
+  document.body.classList.remove("unscrollable");
 }
 
 // Handle click event on menu button
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('open');
+menuBtn.addEventListener("click", () => {
+  let menuBtnClasses = menuBtn.classList;
+ 
+  menuBtnClasses.toggle("open");
+
   const classMenu = mobileMenu.classList;
-  if (classMenu.contains('hide-on-mobile')) {
-    classMenu.replace('hide-on-mobile', 'show-on-mobile');
+  if (classMenu.contains("hide")) {
+    classMenu.replace("hide", "show-on-mobile");
     disableScroll();
-  } else if (classMenu.contains('show-on-mobile')) {
-    classMenu.replace('show-on-mobile', 'hide-on-mobile');
+  } else if (classMenu.contains("show-on-mobile")) {
+    classMenu.replace("show-on-mobile", "hide");
     enableScroll();
   }
   // Handle click event on links from mobile menu;
-  const links = mobileMenu.querySelectorAll('a');
-  links.forEach((l) => l.addEventListener('click', () => {
-    menuBtn.classList.remove('open');
-    classMenu.replace('show-on-mobile', 'hide-on-mobile');
-    enableScroll();
-  }));
+  const links = mobileMenu.querySelectorAll("a");
+  links.forEach((l) =>
+    l.addEventListener("click", () => {
+      menuBtn.classList.remove("open");
+      classMenu.replace("show-on-mobile", "hide");
+      enableScroll();
+    })
+  );
 });
